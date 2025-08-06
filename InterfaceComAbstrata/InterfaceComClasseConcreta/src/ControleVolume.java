d80fd7f8a126ed36cbcbcbf5ea80aac6228478f6
zxDuarte1 <165347640+zxDuarte1@users.noreply.github.com>
2025-08-05 21:47:45 -0300

    Delete InterfaceComAbstrata directory

--- InterfaceComAbstrata/InterfaceComClasseConcreta/src/ControleVolume.java
+++
@@ -1,7 +0,0 @@
-public interface ControleVolume
-{
-	public abstract void maisVolume();
-	public abstract void menosVolume();
-	public abstract void ligarMudo();
-	public abstract void desligarMudo();
-}
