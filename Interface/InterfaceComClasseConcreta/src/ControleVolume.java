229b5334582ed8e76ae943acb4bb11eebf4d4213
zxDuarte1 <165347640+zxDuarte1@users.noreply.github.com>
2025-08-05 21:45:17 -0300

    Add files via upload

---
+++ Interface/InterfaceComClasseConcreta/src/ControleVolume.java
@@ -0,0 +1,7 @@
+public interface ControleVolume
+{
+	public abstract void maisVolume();
+	public abstract void menosVolume();
+	public abstract void ligarMudo();
+	public abstract void desligarMudo();
+}
---
+++ Interface/InterfaceComClasseConcreta/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Controle c = new Controle();
+		
+		c.ligar();
+		c.abrirMenu();
+		c.desligarMudo();
+		c.abrirMenu();
+		c.play();
+		c.abrirMenu();
+		c.maisVolume();
+		c.abrirMenu();
+		c.menosVolume();
+		c.abrirMenu();
+		c.pause();
+		c.abrirMenu();
+		
+		
+	}
+}
